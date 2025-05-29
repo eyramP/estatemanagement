6140a920b2ecc28a967bf31f21598acbfd5ece39
Eyram Cobblah <ecobblah2@gmail.com>
2025-05-29 20:13:34 +0000

    Feature: add TimeStampped, Profile models and signals

--- core_apps/profiles/models.py
+++ core_apps/profiles/models.py
@@ -1,3 +1,94 @@
+from autoslug import AutoSlugField
+from cloudinary.models import CloudinaryField
+from django.contrib.auth import get_user_model
+from django.utils.translation import gettext_lazy as _
+from django_countries.fields import CountryField
+from phonenumber_field.modelfields import PhoneNumberField
 from django.db import models
 
-# Create your models here.
+from core_apps.common.models import TimeStappedModel
+
+User = get_user_model()
+
+
+def get_user_username(instance: "Profile") -> str:
+    return instance.user.username
+
+
+class Profile(TimeStappedModel):
+    class Gender(models.TextChoices):
+        MALE = ("male", _("Male"))
+        FEMALE = ("female", _("Female"))
+
+    class Occupation(models.TextChoices):
+        EMPLOYED = ("employed", _("Employed"))
+        UNEMPLOYED = ("unemployed", _("Unemployed"))
+        CARPENTER = ("carpenter", _("Carpenter"))
+        ELECTRICIAN = ("electrician", _("Electrician"))
+        PLUMBER = ("plumber", _("Plumber"))
+        CIVIL_SERVANT = ("civil_servant", _("Civil Servant"))
+        TENANT = ("tenant", _("Tenant"))
+
+    user = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE,
+        related_name="profile"
+    )
+    avatar = CloudinaryField(
+        verbose_name=_("Avatar"),
+        blank=True,
+        null=True)
+    gender = models.CharField(
+        max_length=10,
+        verbose_name=_("Gender"),
+        choices=Gender.choices,
+        default=Gender.MALE
+    )
+    bio = models.TextField(
+        verbose_name=_("Bio"),
+        blank=True,
+        null=True
+    )
+    occupation = models.CharField(
+        verbose_name=_("Occupation"),
+        max_length=20,
+        choices=Occupation.choices,
+        default=Occupation.TENANT
+    )
+    phone_number = PhoneNumberField(
+        verbose_name=_("Phone Number"),
+        max_length=30,
+        default="+233546789876"
+    )
+    country_of_origin = CountryField(
+        verbose_name=_("Country"),
+        default="GH"
+    )
+    city_of_origin = models.CharField(
+        verbose_name=_("City"),
+        max_length=180,
+        default="Tema"
+    )
+    report_count = models.IntegerField(
+        verbose_name=_("Report Count"),
+        default=0
+    )
+    reputation = models.IntegerField(
+        verbose_name=_("reputation"),
+        default=100
+    )
+    slug = AutoSlugField(
+        populate_from=get_user_username,
+        unique=True
+    )
+
+    @property
+    def is_banned(self) -> bool:
+        return self.report_count >= 5
+
+    def update_reputation(self):
+        self.reputation = max(0, 100 - self.report_count * 20)
+
+    def save(self, *args, **kwargs):
+        self.update_reputation()
+        super().save()
