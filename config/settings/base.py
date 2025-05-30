e80c905662f8a80578ca2bca806360ac199ef8ce
Eyram Cobblah <ecobblah2@gmail.com>
2025-05-30 20:36:10 +0000

    Feature: Add generic renderer and profile celery tasks

--- config/settings/base.py
+++ config/settings/base.py
@@ -248,6 +248,10 @@ CELERY_TASK_SOFT_TIME_LIMIT = 60
 CELERY_BEAT_SCHEDULER = 'django_celery_beat.schedulers:DatabaseScheduler'
 CELERY_WORKERS_SEND_TASK_EVENTS = True
 
+CELERY_BEAT_SCHEDULE = {
+    "update-reputations-every-day": {"task": "update_all_reputations"}
+}
+
 CLOUDINARY_CLOUD_NAME = getenv('CLOUDINARY_CLOUD_NAME')
 CLOUDINARY_API_KEY = getenv('CLOUDINARY_API_KEY')
 CLOUDINARY_API_SECRET = getenv('CLOUDINARY_API_SECRET')
