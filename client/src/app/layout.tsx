f60df6687ab8d134c20189682d030e3c1578b92e
Eyram Cobblah <ecobblah2@gmail.com>
2025-06-10 22:17:06 +0000

    Feature: Setup slices, reducer and provider

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -4,6 +4,7 @@ import React from "react";
 import { openSans, robotoSlab } from "@/lib/fonts";
 import { ThemeProvider } from "@/components/theme-provider";
 
+import ReduxProvider from "@/lib/redux/provider";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -18,12 +19,15 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={`${openSans.variable} ${robotoSlab.variable}`}>
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem disableTransitionOnChange>
-          {children}
-        </ThemeProvider>
+          <ReduxProvider>
+            <ThemeProvider
+              attribute="class"
+              defaultTheme="system"
+              enableSystem disableTransitionOnChange
+            >
+              {children}
+            </ThemeProvider>
+          </ReduxProvider>
         </body>
     </html>
   );
