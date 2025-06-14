70c03e9822f5d0fe80a894ecdde27bf9ffe1a5a7
Eyram Cobblah <ecobblah2@gmail.com>
2025-06-14 08:23:39 +0000

    Feature: Add re-authorization with async-mutex

---
+++ client/src/app/(estate)/profile/page.tsx
@@ -0,0 +1,21 @@
+"use client"
+import Spinner from "@/components/shared/spinner"
+import { useGetUserProfileQuery } from "@/lib/redux/features/users/usersApiSlice"
+
+export default function ProfilePage() {
+  const {data, isLoading} = useGetUserProfileQuery()
+
+  if (isLoading){
+          return (
+              <div className="flex-center pt-32">
+                  <Spinner size="xl"></Spinner>
+              </div>
+          )
+      }
+
+    return (
+    <div>
+        <h1>{data?.profile.username}&apos;s Profile</h1>
+    </div>
+  )
+}
---
+++ client/src/lib/redux/features/users/usersApiSlice.ts
@@ -0,0 +1,62 @@
+import { baseApiSlice } from "@/lib/redux/features/api/baseApiSlice";
+import { NonTenantResponse, ProfileData, ProfileResponse, ProfilesResponse, QueryParams } from "@/types";
+import build from "next/dist/build";
+import { useGetUserQuery } from "../auth/authApiSlice";
+
+export const usersApiSlice = baseApiSlice.injectEndpoints({
+    endpoints: (builder) => ({
+        getAllUsers: builder.query<ProfilesResponse, QueryParams>({
+            query: (params = {}) => {
+                const queryString = new URLSearchParams()
+
+                if (params.page){
+                    queryString.append("page", params.page.toString())
+                }
+                if (params.searchTerm){
+                    queryString.append("search", params.searchTerm)
+                }
+
+                return `/profiles/all/?${queryString.toString()}`
+            },
+            providesTags:["User"]
+        }),
+
+        getAllTechnicians: builder.query<NonTenantResponse, QueryParams>({
+            query:(params = {}) => {
+                const queryString = new URLSearchParams()
+
+                if (params.page){
+                    queryString.append("page", params.page.toString())
+                }
+                if (params.searchTerm){
+                    queryString.append("search", params.searchTerm)
+                }
+
+                return `/profiles/non-tenants-profiles/?${queryString.toString()}`
+            },
+            providesTags:["User"]
+        }),
+
+        getUserProfile: builder.query<ProfileResponse, void>({
+            query: () => "/profiles/user/my-profile",
+            providesTags: ["User"]
+        }),
+
+        updateUserProfile: builder.mutation<ProfileData, ProfileData>({
+            query:(formData) => ({
+                url:"/profiles/user/update",
+                method: "Path",
+                body: formData
+            }),
+            invalidatesTags: ["User"]
+        })
+    })
+})
+
+export const {
+    useGetAllUsersQuery,
+    useGetAllTechniciansQuery,
+    useGetUserProfileQuery,
+    useLazyGetUserProfileQuery,
+    useUpdateUserProfileMutation
+} = usersApiSlice
