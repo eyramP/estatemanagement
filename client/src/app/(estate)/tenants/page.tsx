d8e494f0852f2b6e46966775b64f3218f378cd85
Eyram Cobblah <ecobblah2@gmail.com>
2025-06-18 12:36:33 +0000

    Feature: Add tenants card with basic info

--- client/src/app/(estate)/tenants/page.tsx
+++ client/src/app/(estate)/tenants/page.tsx
@@ -1,40 +1,25 @@
-"use client"
+import TenantCard from "@/components/cards/TenantCard";
 import ProtectedRoute from "@/components/shared/ProtectedRoutes"
-import Spinner from "@/components/shared/spinner"
-import { useGetAllUsersQuery } from "@/lib/redux/features/users/usersApiSlice"
+import type { Metadata } from "next";
 
-function TenantsPageContent() {
-    const {data, isLoading} = useGetAllUsersQuery({})
-
-    if (isLoading){
-        return (
-            <div className="flex-center pt-32">
-                <Spinner size="xl" />
-            </div>
-        )
-    }
-  return (
-    <div>
-        <h1 className="dark:text-pumpkin text-6xl">Tenants</h1>
-        {data  && data.profiles.results.length > 0 ? (
-            data.profiles.results.map((tenant) => (
-                <p
-                key={tenant.id}
-                className="text-2xl dark:text-lime-500">
-                    {tenant.full_name} - {tenant.occupation}
-                </p>
-            ))
-        ): (
-            <p>No tenants found.</p>
-        )}
-    </div>
-  )
+export const metadata: Metadata = {
+    title: "Estate Management",
+    description: "Authenticated users can view basic information about other tenanst within the property. Tenants can also search other tenants."
 }
+
+function TenantPageContent (){
+    return (
+    <div>
+        <TenantCard />
+    </div>
+    )
+}
+
 
 export default function TenantsPage(){
     return (
         <ProtectedRoute>
-            <TenantsPageContent />
+            <TenantPageContent />
         </ProtectedRoute>
     )
 }
---
+++ client/src/components/cards/TenantInfo.tsx
@@ -0,0 +1,15 @@
+interface TenantInfoProps {
+    label: string;
+    value: string | number;
+    icon: any
+}
+
+function TenantInfo({label, value, icon: Icon}: TenantInfoProps){
+    return <p className="flex items-center space-x-2 space-y-1">
+        <Icon className="card-icon" />
+        <span className="tab-font">{label}:</span>
+        <span className="font-tab">{value}</span>
+    </p>
+}
+
+export default TenantInfo;
